1bf516e540e295536944e0ad57284a2fded6da9e
AquaVitSaMa <2924270322@qq.com>
2021-10-29 03:52:30 +0800

    优化ItemPhysic

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRenderEntityItem.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/render/MixinRenderEntityItem.java
@@ -25,6 +25,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
@@ -32,50 +35,74 @@ import java.util.Random;
 public abstract class MixinRenderEntityItem extends MixinRender{
 
     @Shadow
-    private Random field_177079_e = new Random();
-
-    @Shadow
-    @Final
-    private RenderItem itemRenderer;
-
-    @Shadow
-    public abstract boolean shouldSpreadItems();
-
-    @Shadow
-    protected abstract ResourceLocation getEntityTexture(EntityItem p_getEntityTexture_1_);
-
-    @Shadow
     protected abstract int func_177078_a(ItemStack p_177078_1_);
 
-    @Shadow
-    public abstract boolean shouldBob();
+    private float ItemToRotate;
 
     /**
      * @author AquaVit QQ:2924270322
      */
     @Overwrite
-    private int func_177077_a(EntityItem p_177077_1_, double p_177077_2_, double p_177077_4_, double p_177077_6_, float p_177077_8_, IBakedModel p_177077_9_){
-        ItemStack itemstack = p_177077_1_.getEntityItem();
+    private int func_177077_a(EntityItem itemIn, double p_177077_2_, double p_177077_4_, double p_177077_6_, float p_177077_8_, IBakedModel p_177077_9_) {
+
+        ItemStack itemstack = itemIn.getEntityItem();
         Item item = itemstack.getItem();
+
         if (item == null) {
             return 0;
         } else {
             boolean flag = p_177077_9_.isGui3d();
             int i = this.func_177078_a(itemstack);
             float f = 0.25F;
-            float f1 = this.shouldBob() ? MathHelper.sin(((float)p_177077_1_.getAge() + p_177077_8_) / 10.0F + p_177077_1_.hoverStart) * 0.1F + 0.1F : 0.0F;
-            float f2 = p_177077_9_.getItemCameraTransforms().getTransform(ItemCameraTransforms.TransformType.GROUND).scale.y;
-            GlStateManager.translate((float)p_177077_2_, (float)p_177077_4_ + f1 + 0.25F * f2, (float)p_177077_6_);
-            float f6;
+
+            boolean flagNoHover = !LiquidBounce.moduleManager.getModule(ItemPhysic.class).getState() || !ItemPhysic.nohover.get();
+            float f1;
+
+            if (flagNoHover) {
+                f1 = MathHelper.sin(((float) itemIn.getAge() + p_177077_8_) / 10.0F + itemIn.hoverStart) * 0.1F + 0.1F;
+            } else {
+                f1 =  flag ? -0.1f : -0.23f;
+            }
+
+            float f2 = p_177077_9_.getItemCameraTransforms()
+                    .getTransform(ItemCameraTransforms.TransformType.GROUND).scale.y;
+            GlStateManager.translate((float) p_177077_2_, (float) p_177077_4_ + f1 + 0.25F * f2, (float) p_177077_6_);
+
             if (flag || this.renderManager.options != null) {
-                f6 = (((float)p_177077_1_.getAge() + p_177077_8_) / 20.0F + p_177077_1_.hoverStart) * 57.295776F;
-                GlStateManager.rotate(f6, 0.0F, 1.0F, 0.0F);
+
+                float f3 = (((float) itemIn.getAge() + p_177077_8_) / 20.0F + itemIn.hoverStart)
+                        * (180F / (float) Math.PI);
+
+                if(!flagNoHover)
+                    f3 = itemIn.hoverStart + itemIn.getEntityId();
+
+                GlStateManager.rotate(f3, 0.0F, 1.0F, 0.0F);
+            }
+
+            if (LiquidBounce.moduleManager.getModule(ItemPhysic.class).getState()) {
+
+                if (!itemIn.onGround)
+                    ItemToRotate += 4.0f;
+
+            } else {
+
+                if (ItemToRotate != 0.0F)
+                    ItemToRotate = 0.0F;
+            }
+
+            if (ItemToRotate > 0.0F) {
+                if (!itemIn.onGround) {
+                    GlStateManager.rotate(ItemToRotate, ItemPhysic.rotateX.get() ? 360F : 0F, ItemPhysic.rotateY.get() ? 360F : 0F, ItemPhysic.rotateZ.get() ? 360F : 0F);
+                } else {
+                    GlStateManager.scale(1.2F, 1.2F, 1.2F);
+                    GlStateManager.rotate(90F, 90F, 0F, 0F);
+                }
             }
 
             if (!flag) {
-                f6 = -0.0F * (float)(i - 1) * 0.5F;
-                float f4 = -0.0F * (float)(i - 1) * 0.5F;
-                float f5 = -0.046875F * (float)(i - 1) * 0.5F;
+                float f6 = -0.0F * (float) (i - 1) * 0.5F;
+                float f4 = -0.0F * (float) (i - 1) * 0.5F;
+                float f5 = -0.046875F * (float) (i - 1) * 0.5F;
                 GlStateManager.translate(f6, f4, f5);
             }
 
@@ -84,79 +111,6 @@ public abstract class MixinRenderEntityItem extends MixinRender{
         }
     }
 
-
-    /**
-     * @author AquaVit QQ:2924270322
-     */
-    @Overwrite
-    public void doRender(EntityItem entity, double x, double y, double z, float entityYaw, float partialTicks) {
-        final Chams chams = (Chams) LiquidBounce.moduleManager.getModule(Chams.class);
-
-        if (chams.getState() && chams.getItemsValue().get()) {
-            GL11.glEnable(GL11.GL_POLYGON_OFFSET_FILL);
-            GL11.glPolygonOffset(1.0F, -1000000F);
-        }
-
-        ItemStack itemstack = entity.getEntityItem();
-        this.field_177079_e.setSeed(187L);
-        boolean flag = false;
-        if (this.bindEntityTexture(entity)) {
-            this.renderManager.renderEngine.getTexture(this.getEntityTexture(entity)).setBlurMipmap(false, false);
-            flag = true;
-        }
-
-        if (LiquidBounce.moduleManager.getModule(ItemPhysic.class).getState()){
-            ItemPhysicUtil.doRenderItemPhysic(entity, x, y, z, entityYaw, partialTicks);
-            if (chams.getState() && chams.getItemsValue().get()) {
-                GL11.glPolygonOffset(1.0F, 1000000F);
-                GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL);
-            }
-            return;
-        }
-
-        GlStateManager.enableRescaleNormal();
-        GlStateManager.alphaFunc(516, 0.1F);
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-        GlStateManager.pushMatrix();
-        IBakedModel ibakedmodel = this.itemRenderer.getItemModelMesher().getItemModel(itemstack);
-        int i = this.func_177077_a(entity, x, y, z, partialTicks, ibakedmodel);
-
-        for(int j = 0; j < i; ++j) {
-            GlStateManager.pushMatrix();
-            if (j > 0) {
-                float f = (this.field_177079_e.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                float f1 = (this.field_177079_e.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                float f2 = (this.field_177079_e.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                GlStateManager.translate(this.shouldSpreadItems() ? f : 0.0F, this.shouldSpreadItems() ? f1 : 0.0F, f2);
-            }
-
-            if (ibakedmodel.isGui3d()) {
-                GlStateManager.scale(0.5F, 0.5F, 0.5F);
-            }
-
-            ibakedmodel = ForgeHooksClient.handleCameraTransforms(ibakedmodel, ItemCameraTransforms.TransformType.GROUND);
-            this.itemRenderer.renderItem(itemstack, ibakedmodel);
-            GlStateManager.popMatrix();
-        }
-
-        GlStateManager.popMatrix();
-        GlStateManager.disableRescaleNormal();
-        GlStateManager.disableBlend();
-        this.bindEntityTexture(entity);
-        if (flag) {
-            this.renderManager.renderEngine.getTexture(this.getEntityTexture(entity)).restoreLastBlurMipmap();
-        }
-
-        if (chams.getState() && chams.getItemsValue().get()) {
-            GL11.glPolygonOffset(1.0F, 1000000F);
-            GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL);
-        }
-
-        super.doRender(entity, x, y, z, entityYaw, partialTicks);
-    }
-
-    /*
     @Inject(method = "doRender", at = @At("HEAD"))
     private void injectChamsPre(CallbackInfo callbackInfo) {
         final Chams chams = (Chams) LiquidBounce.moduleManager.getModule(Chams.class);
@@ -177,5 +131,4 @@ public abstract class MixinRenderEntityItem extends MixinRender{
             GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL);
         }
     }
-     */
 }
