8cd47b436f0d17f0fcd98f00763d4147bd561e41
AquaVitSaMa <2924270322@qq.com>
2021-11-06 05:37:01 +0800

    ModuleCategory kt to java

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/ModuleCategory.java
@@ -0,0 +1,23 @@
+package net.ccbluex.liquidbounce.features.module;
+
+public enum ModuleCategory
+{
+    COMBAT("Combat"),
+    PLAYER("Player"),
+    MOVEMENT("Movement"),
+    RENDER("Render"),
+    WORLD("World"),
+    MISC("Misc"),
+    EXPLOIT("Exploit"),
+    FUN("Fun");
+
+    public final String displayName;
+
+    ModuleCategory(final String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
+}
