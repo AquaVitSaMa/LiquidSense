f1fec4076226119f34d73be777b42e994cc1e04f
AquaVitSaMa <2924270322@qq.com>
2021-11-21 20:38:30 +0800

    移动Antiforge

--- src/main/java/net/ccbluex/liquidbounce/ui/client/gui/GuiAntiForge.java
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/gui/GuiAntiForge.java
@@ -3,7 +3,7 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/CCBlueX/LiquidBounce/
  */
-package net.ccbluex.liquidbounce.ui.client;
+package net.ccbluex.liquidbounce.ui.client.gui;
 
 import net.ccbluex.liquidbounce.LiquidBounce;
 import net.ccbluex.liquidbounce.features.special.AntiForge;
