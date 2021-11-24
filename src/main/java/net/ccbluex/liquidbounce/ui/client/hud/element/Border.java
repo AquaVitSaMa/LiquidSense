cda4c472e83c5b5ce0fc4765cb9d5f06032c505a
我是脑残 <727819556@qq.com>
2021-11-24 22:00:46 +0800

    修复组件坐标算法错误

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/Border.java
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/Border.java
@@ -4,29 +4,13 @@ import net.ccbluex.liquidbounce.utils.render.RenderUtils;
 
 public class Border {
 
-    private float x;
-    private float y;
-    private float x2;
-    private float y2;
+	public float x;
+	public float y;
+	public float x2;
+	public float y2;
 
     public void draw() {
         RenderUtils.drawBorderedRect(this.x, this.y, this.x2, this.y2, 3.0F, Integer.MIN_VALUE, 0);
-    }
-
-    public final float getX() {
-        return this.x;
-    }
-
-    public final float getY() {
-        return this.y;
-    }
-
-    public final float getX2() {
-        return this.x2;
-    }
-
-    public final float getY2() {
-        return this.y2;
     }
 
     public Border(float x, float y, float x2, float y2) {
