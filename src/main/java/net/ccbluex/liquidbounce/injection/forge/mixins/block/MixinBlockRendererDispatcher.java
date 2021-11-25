e5f4ac47344f468923aa88ab9f58377ff6814a32
AquaVitSaMa <2924270322@qq.com>
2021-11-26 02:17:52 +0800

    新增MixinBlockRendererDispatcher和RenderBlockEvent

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/block/MixinBlockRendererDispatcher.java
@@ -0,0 +1,27 @@
+package net.ccbluex.liquidbounce.injection.forge.mixins.block;
+
+import net.ccbluex.liquidbounce.LiquidBounce;
+import net.ccbluex.liquidbounce.event.events.RenderBlockEvent;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockRendererDispatcher.class)
+@SideOnly(Side.CLIENT)
+public class MixinBlockRendererDispatcher {
+
+    @Inject(method = "renderBlock", at = @At("HEAD"), cancellable = true)
+    public void renderBlock(IBlockState state, BlockPos pos, IBlockAccess blockAccess, WorldRenderer worldRendererIn, final CallbackInfoReturnable<Boolean> info) {
+        RenderBlockEvent event = new RenderBlockEvent(pos.getX(), pos.getY(), pos.getZ(), state.getBlock(), pos);
+        LiquidBounce.eventManager.callEvent(event);
+    }
+
+}
