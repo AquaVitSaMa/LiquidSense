0c8198c493ca62d73669d683e74130631e616854
AquaVitSaMa <2924270322@qq.com>
2021-12-02 02:50:58 +0800

    FastUse kt to java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/entity/MixinEntityPlayer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/entity/MixinEntityPlayer.java
@@ -1,27 +1,15 @@
-/*
- * LiquidBounce Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/CCBlueX/LiquidBounce/
- */
 package net.ccbluex.liquidbounce.injection.forge.mixins.entity;
 
 import com.mojang.authlib.GameProfile;
-import net.ccbluex.liquidbounce.LiquidBounce;
-import net.ccbluex.liquidbounce.features.module.modules.player.FastUse;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.PlayerCapabilities;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.FoodStats;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityPlayer.class)
 public abstract class MixinEntityPlayer extends MixinEntityLivingBase {
-
-    final FastUse fastuse = (FastUse) LiquidBounce.moduleManager.getModule(FastUse.class);
 
     @Shadow
     public abstract ItemStack getHeldItem();
@@ -52,12 +40,4 @@ public abstract class MixinEntityPlayer extends MixinEntityLivingBase {
 
     @Shadow
     public abstract boolean isUsingItem();
-
-
-    @Inject(method = "clearItemInUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/EntityPlayer;setEating(Z)V", shift = At.Shift.AFTER))
-    private void clearItemInUse(CallbackInfo callbackInfo) {
-        if (LiquidBounce.moduleManager.getModule(FastUse.class).getState()) {
-            ((FastUse)LiquidBounce.moduleManager.getModule(FastUse.class)).resetTimer();
-        }
-    }
 }
